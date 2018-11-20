d13b74b0aacb80faa469a90585e08be68e8afaf7
GreaterFire <32649575+GreaterFire@users.noreply.github.com>
2018-11-19 20:11:49 -0800

    Add n2s file

---
+++ src/n2t/n2s.h
@@ -0,0 +1,23 @@
+/*
+ * This file is part of the libn2t project.
+ * Libn2t is a C++ library transforming network layer into transport layer.
+ * Copyright (C) 2018  GreaterFire
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef _N2T_N2S_H_
+#define _N2T_N2S_H_
+
+#endif // _N2T_N2S_H_
