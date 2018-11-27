91a5068bbb3d36b95f94ffdd94b27906cb75df19
GreaterFire <32649575+GreaterFire@users.noreply.github.com>
2018-11-26 23:35:45 -0800

    remove unnecessary include in lwipopts

--- src/lwipopts.h
+++ src/lwipopts.h
@@ -39,13 +39,6 @@
 #define LWIP_LWIPOPTS_H
 
 /*
- * Include user defined options first. Anything not defined in these files
- * will be set to standard values. Override anything you dont like!
- */
-#include "lwipopts.h"
-#include "lwip/debug.h"
-
-/*
    -----------------------------------------------
    ---------- Platform specific locking ----------
    -----------------------------------------------
